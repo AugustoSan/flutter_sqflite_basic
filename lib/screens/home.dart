1d5dfa68b761501d50a37ab03b2961add4b23b18
AugustoSan <augustosanchez103@gmail.com>
2022-05-08 16:01:51 -0500

    se quita linea de impor que no se usaba

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sqflite_basic/models/password_model.dart';
 import 'package:flutter_sqflite_basic/providers/password_provider.dart';
 import 'package:provider/provider.dart';
 
